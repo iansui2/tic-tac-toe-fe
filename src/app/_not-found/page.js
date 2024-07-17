060be73b40a2c395d67780f2a031cfc5eba320b9
Ronan Ian D. Sui <39039775+iansui2@users.noreply.github.com>
2024-07-17 10:50:31 +0800

    fix issue on building nextjs proj

---
+++ src/app/_not-found/page.js
@@ -0,0 +1,6 @@
+// app/not-found/page.js
+'use client'
+
+export default function NotFound() {
+  return <h1>404 - Page Not Found</h1>;
+}
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,23 +1,11 @@
 // app/providers.tsx
 'use client'
 
-import { ChakraProvider, useColorMode } from '@chakra-ui/react';
-
-const DefaultColorMode = () => {
-  const { colorMode, setColorMode } = useColorMode();
-
-  // Set default color mode to light
-  if (colorMode === 'light') {
-    setColorMode('light');
-  }
-
-  return null; // This component doesn't render anything
-};
+import { ChakraProvider } from '@chakra-ui/react';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ChakraProvider>
-      <DefaultColorMode />
       {children}
     </ChakraProvider>
   );
